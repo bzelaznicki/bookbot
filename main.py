6a765e59f3c320c4cf3bbad984b15304dcd8cc33
bzelaznicki <bartosz@zelaznicki.com>
2024-11-05 20:28:57 +0100

    add character counts

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = read_book(book_path)
     word_count = count_words(book_text)
-    print(f"{word_count} characters in the book")
+    character_count = count_characters(book_text)
+    print(f"{word_count} words in the book")
+    print(f"{character_count}")
 
 def read_book(book_path):
     with open(book_path) as f:
@@ -11,7 +13,18 @@ def read_book(book_path):
 
 def count_words(text):
     split_text = text.split()
-    character_count = len(split_text)
-    return character_count
+    word_count = len(split_text)
+    return word_count
+
+def count_characters(text):
+    lowered_text = text.lower()
+    characters = list(lowered_text)
+    character_counts = {}
+    for character in characters:
+        if character in character_counts:    
+            character_counts[character] += 1
+        else:
+            character_counts[character] = 1
+    return character_counts
 
 main()
